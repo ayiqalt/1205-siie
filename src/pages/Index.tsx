22731713c958b515a9923b2f2eacdd8630233a0b
ayiqalt <ayiqalt@gmail.com>
2025-05-12 23:35:56 -0300

    Add files via upload

---
+++ src/pages/Index.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/components/Hero";
+import MenuSection from "@/components/MenuSection";
+import Navbar from "@/components/Navbar";
+
+const Index = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <MenuSection />
+    </div>
+  );
+};
+
+export default Index;
